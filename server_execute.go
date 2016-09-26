48201c03b2e3faad8e0b125dddd9510636a61751
Michael O'Keefe <mokeefe@mitre.org>
2016-09-26 11:43:57 -0400

    server execute comment out

--- server_execute.go
+++ server_execute.go
@@ -1,14 +1,14 @@
-package main
+// package main
 
-import (
-	"github.com/intervention-engine/fhir/auth"
-	"github.com/intervention-engine/fhir/server"
-)
+// import (
+// 	"github.com/intervention-engine/fhir/auth"
+// 	"github.com/intervention-engine/fhir/server"
+// )
 
-func main() {
-	mongoURL := "mongodb://localhost:27017"
+// func main() {
+// 	mongoURL := "mongodb://localhost:27017"
 
-	s := server.NewServer(mongoURL)
+// 	s := server.NewServer(mongoURL)
 
-	s.Run(server.Config{Auth: auth.None()})
-}
+// 	s.Run(server.Config{Auth: auth.None()})
+// }
